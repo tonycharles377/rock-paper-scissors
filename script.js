9f7a19f0dfc64a169a4878738e2204da45f9f600
tonycharles377 <tonycharles377@gmail.com>
2023-10-20 12:38:58 +0300

    Add function to choose a pick for human

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 function getComputerChoice(){
     let options = ['rock', 'paper', 'scissors'];
     let choice = options[Math.floor(Math.random() * options.length)];
-    
+
     console.log(choice);
 }
 
 getComputerChoice();
+
+//human's pick
+function getHumanChoise(){
+    let choice = prompt('Pick an option: Rock, Paper, Scissors').toLocaleLowerCase();
+
+    console.log(choice);
+}
+
+getHumanChoise();
