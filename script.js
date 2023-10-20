1e6b8d064c92387eca2e694b18fde7dd1e88fa13
tonycharles377 <tonycharles377@gmail.com>
2023-10-20 17:13:43 +0300

    Single play round game function

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
 //computer's pick
 function getComputerChoice(){
     let options = ['rock', 'paper', 'scissors'];
-    let choice = options[Math.floor(Math.random() * options.length)];
+    let computerSelection = options[Math.floor(Math.random() * options.length)];
 
-    console.log(choice);
+    return computerSelection;
 }
-
-getComputerChoice();
 
 //human's pick
 function getHumanChoise(){
-    let choice = prompt('Pick an option: Rock, Paper, Scissors').toLocaleLowerCase();
+    let playerSelection = prompt('Pick an option: Rock, Paper, Scissors').toLocaleLowerCase();
 
-    console.log(choice);
+    return playerSelection;
 }
 
-getHumanChoise();
+//single play-round game function
+function singleRound(computerSelection, playerSelection){
+    if (computerSelection === playerSelection) {
+        console.log('you tie computer: ' + computerSelection + ' vs player: ' + playerSelection);
+    } else {
+        switch (computerSelection) {
+            case 'rock':
+                switch (playerSelection) {
+                    case 'paper':
+                        console.log('you win! ' + playerSelection + ' beats ' + computerSelection);
+                        break;
+                    case 'scissors':
+                        console.log('you lose! ' + computerSelection + ' beats ' + playerSelection);
+                        break;
+                }
+                break;
+            case 'paper':
+                switch (playerSelection) {
+                    case 'scissors':
+                        console.log('you win! ' + playerSelection + ' beats ' + computerSelection);
+                        break;
+                    case 'rock':
+                        console.log('you lose! ' + computerSelection + ' beats ' + playerSelection);
+                        break;
+                }
+                break;
+            case 'scissors':
+                switch (playerSelection) {
+                    case 'rock':
+                        console.log('you win! ' + playerSelection + ' beats ' + computerSelection);
+                        break;
+                    case 'paper':
+                        console.log('you lose! ' + computerSelection + ' beats ' + playerSelection);
+                        break;
+                }
+                break;
+            default:
+                console.log('Invalid selection');
+        }
+    }
+}
+
+singleRound(getComputerChoice(), getHumanChoise());
+
