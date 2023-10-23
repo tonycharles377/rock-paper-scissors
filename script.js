3359d0e549ef27363f3b77c056188f06736cc167
tonycharles377 <tonycharles377@gmail.com>
2023-10-23 12:43:37 +0300

    Add five rouns-play function

--- script.js
+++ script.js
@@ -13,47 +13,48 @@ function getHumanChoise(){
     return playerSelection;
 }
 
-//single play-round game function
-function singleRound(computerSelection, playerSelection){
-    if (computerSelection === playerSelection) {
-        console.log('you tie computer: ' + computerSelection + ' vs player: ' + playerSelection);
-    } else {
-        switch (computerSelection) {
-            case 'rock':
-                switch (playerSelection) {
-                    case 'paper':
-                        console.log('you win! ' + playerSelection + ' beats ' + computerSelection);
-                        break;
-                    case 'scissors':
-                        console.log('you lose! ' + computerSelection + ' beats ' + playerSelection);
-                        break;
-                }
-                break;
-            case 'paper':
-                switch (playerSelection) {
-                    case 'scissors':
-                        console.log('you win! ' + playerSelection + ' beats ' + computerSelection);
-                        break;
-                    case 'rock':
-                        console.log('you lose! ' + computerSelection + ' beats ' + playerSelection);
-                        break;
-                }
-                break;
-            case 'scissors':
-                switch (playerSelection) {
-                    case 'rock':
-                        console.log('you win! ' + playerSelection + ' beats ' + computerSelection);
-                        break;
-                    case 'paper':
-                        console.log('you lose! ' + computerSelection + ' beats ' + playerSelection);
-                        break;
-                }
-                break;
-            default:
-                console.log('Invalid selection');
+
+
+
+//five rounds-play game function
+function game(){
+    
+
+    let player = 0;
+    let computer = 0;
+
+    while(player < 5 || computer < 5){
+        singleRound(getComputerChoice(), getHumanChoise());
+        if(player == 4){
+            console.log('You win!');
+            break;
+        }else if (computer == 4){
+            console.log('You lose!');
+            break;
         }
     }
+
+    //single play-round game function
+    function singleRound(computerSelection, playerSelection){
+        if (computerSelection === playerSelection) {
+            console.log('you tie computer: ' + computerSelection + ' vs player: ' + playerSelection);
+            alert('player: 0 computer: 0');
+        }
+        else if(playerSelection === 'rock' && computerSelection === 'scissors' ||
+                playerSelection === 'paper' && computerSelection === 'rock' ||
+                playerSelection === 'scissors' && computerSelection === 'rock'){
+                    console.log('You win! ' + playerSelection + ' beat ' + computerSelection);
+                    alert('player: 1 computer: 0');
+                    player++;
+        }
+        else{
+            console.log('You losse! ' + computerSelection + ' beat ' + playerSelection);
+            alert('player: 0 computer: 1');
+            computer++;
+        }
+    }
+
 }
 
-singleRound(getComputerChoice(), getHumanChoise());
-
+game();
+                        
